57bc80aeb2c2c123d9a855988561c5c50242c23e
Joe MacMahon <joe.macmahon@cern.ch>
2018-03-19 21:45:07 +0100

    Suppress duplicate tweet errors

--- nationalise_every_word/tweet.py
+++ nationalise_every_word/tweet.py
@@ -7,4 +7,9 @@ api = Api(**config['keys'])
 
 def write_tweet(word):
     tweet_body = config['format'].format(word)
-    api.PostUpdate(tweet_body)
+    try:
+        api.PostUpdate(tweet_body)
+    except TwitterError as e:
+        # Suppress duplicate tweet errors
+        if e.message[0]['code'] != 187:
+            raise
