2624765c5077c034882f86ad13ec037fb27cfa79
Joe MacMahon <joe@macmahon.eu>
2018-03-19 20:55:56 +0000

    Added MaxRetryError handling

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ from nationalise_every_word import words_gen, write_tweet, config
 from time import sleep
 from sys import exc_info
 from traceback import format_exception
+from urllib3.exceptions import MaxRetryError
 
 
 def run():
     for (word_index, word) in words_gen():
-        try:
-            write_tweet(word)
-            print('Tweeted word: {}'.format(word))
-        except Exception as e:
-            message = ('Caught an exception on word {} ' +
-                       'at index {}:').format(word, word_index)
-            log = format_exception(*exc_info())
-            with open(config['error_log'], 'a') as error_log:
-                error_log.writelines((message, log))
-            print((message, log))
-        finally:
-            sleep(config['interval'])
+        success = False
+        while not success:
+            try:
+                write_tweet(word)
+                print('Tweeted word: {}'.format(word))
+                success = True
+            except MaxRetryError:
+                sleep(config['interval'])
+            except Exception as e:
+                message = ('Caught an exception on word {} ' +
+                           'at index {}:').format(word, word_index)
+                log = format_exception(*exc_info())
+                with open(config['error_log'], 'a') as error_log:
+                    error_log.writelines((message, log))
+                print((message, log))
+        sleep(config['interval'])
 
 
 if __name__ == "__main__":
